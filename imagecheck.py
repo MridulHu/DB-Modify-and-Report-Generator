7bc5e60b4908d36aa64f698bf8e55b6e5c4d1898
MridulHu <141259868+MridulHu@users.noreply.github.com>
2024-07-24 22:52:44 +0530

    Delete imagecheck.py

--- imagecheck.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtGui import QPalette, QBrush, QPixmap
-image_path = "C:/Project/background.jpg"
-pixmap = QPixmap(image_path)
-if pixmap.isNull():
-    print("Image failed to load.")
-else:
-    print("Image loaded successfully.")
